12c03bac47ed95c2dd3926845a7d6c4449dfc809
mothibimalebana <mothibi.mm12@gmail.com>
2024-04-07 01:40:24 +0200

    passed test 1/7 under repeatString.js
    The function can repeat the string, this was tested using repeatString('hey', 3)

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let concatedString = string;
 
+    for(i = 1; i < num; i++) {
+        concatedString += string;
+    }
+
+    return concatedString
 };
 
+repeatString();
 // Do not edit below this line
 module.exports = repeatString;
