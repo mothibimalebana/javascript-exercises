1f13e5f21101b04140fa636d13759e13e9614359
mothibimalebana <mothibi.mm12@gmail.com>
2024-04-09 13:48:01 +0200

    passed 1/6 tests under sumAll
    sumAll function sums numbers within the range (2ms)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+/**
+ * @u - initial value
+ * @v - final value
+ */
+const sumAll = function(u, v) {
+    let i, partialSum;
+    partialSum = 0;
 
+    for(i = u; i <= 4; i++)
+        partialSum += i;
+
+    return partialSum
 };
 
 // Do not edit below this line
