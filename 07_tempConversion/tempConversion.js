7b8d4d5935d21f95f7399e90e5b8c82130d43e53
mothibimalebana <mothibi.mm12@gmail.com>
2024-04-13 23:25:56 +0200

    passed 1/6 under 07_tempConversion
    works for convertToCelsius(32)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  tempFahrenheit -= 32;
+  tempFahrenheit /= 2;
+
+  return tempFahrenheit
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  tempCelsius *= 1.8;
+  tempCelsius += 32;
+
+  return tempCelsius
 };
 
 // Do not edit below this line
